16887c790e8acd985e121a0511b6140df0410f84
Francis Pang <boyshawn@hotmail.com>
2018-09-07 08:21:52 +0800

    Add source and testing for Data Structure Chapter 1 Arrays & String 1.4 Palindrome Permutation

---
+++ src/test/java/cracking_the_coding_interview/PalindromePermutationTest.java
@@ -0,0 +1,42 @@
+package cracking_the_coding_interview;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class PalindromePermutationTest {
+	static PalindromePermutation palindromePermutation;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		palindromePermutation = new PalindromePermutation();
+	}
+
+	@Test
+	final void testEmptyString() {
+		assertTrue(palindromePermutation.isStringAPermutationOfPalindrome(""));
+	}
+	
+	@Test
+	final void testLengthOneString() {
+		assertTrue(palindromePermutation.isStringAPermutationOfPalindrome("z"));
+	}
+	
+	@Test
+	final void testLengthTwoString() {
+		assertFalse(palindromePermutation.isStringAPermutationOfPalindrome("ab"));
+	}
+	
+	@Test
+	final void testOddLengthString() {
+		assertTrue(palindromePermutation.isStringAPermutationOfPalindrome("aa  cc  b"));
+		assertFalse(palindromePermutation.isStringAPermutationOfPalindrome("ab,,,caa"));
+	}
+	
+	@Test
+	final void testEvenLengthString() {
+		assertTrue(palindromePermutation.isStringAPermutationOfPalindrome("dfdf  a,a"));
+		assertFalse(palindromePermutation.isStringAPermutationOfPalindrome("abcabd"));
+	}
+}
