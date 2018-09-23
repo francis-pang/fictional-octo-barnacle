7857955fd3140958e0adb5aced15e8bfc8d34974
Francis Pang <boyshawn@hotmail.com>
2018-09-23 18:28:03 +0800

    Concepts and Algorithms Chapter 5 Bit Manipulation 5.3 Flip to Win

---
+++ src/test/java/cracking_the_coding_interview/bit_manipulation/FlipBitToWinTest.java
@@ -0,0 +1,56 @@
+package cracking_the_coding_interview.bit_manipulation;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlipBitToWinTest {
+	static FlipBitToWin flipBitToWin;
+
+	@BeforeAll
+	static void setUp() {
+		flipBitToWin = new FlipBitToWin();
+	}
+
+	@Test
+	void testSingleOne() {
+		assertEquals(1, flipBitToWin.longestSequenceOfOnes(1));
+	}
+
+	@Test
+	void testSingleZero(){
+		assertEquals(1, flipBitToWin.longestSequenceOfOnes(0));
+	}
+
+	@Test
+	void testAllOnes() {
+		assertEquals(5, flipBitToWin.longestSequenceOfOnes(Integer.parseInt("11111",2)));
+	}
+
+	@Test
+	void testEndsWithOne(){
+		assertEquals(4, flipBitToWin.longestSequenceOfOnes(Integer.parseInt("1011", 2)));
+	}
+
+	@Test
+	void testEndsWithZero(){
+		assertEquals(4, flipBitToWin.longestSequenceOfOnes(Integer.parseInt("10110",2)));
+	}
+
+	@Test
+	void testDoubleZero(){
+		assertEquals(3, flipBitToWin.longestSequenceOfOnes(Integer.parseInt("10011", 2)));
+	}
+
+	@Test
+	void testDoubleZeroEndZero(){
+		assertEquals(3, flipBitToWin.longestSequenceOfOnes(Integer.parseInt("100110", 2)));
+	}
+
+	@Test
+	void testComplexCase(){
+		assertEquals(4, flipBitToWin.longestSequenceOfOnes(Integer.parseInt("10011010100110", 2)));
+	}
+}
