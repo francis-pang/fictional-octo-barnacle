11de764f4bfb8a49f3d0b4a2570466e58ba43b17
boyshawn <boyshawn@hotmail.com>
2019-03-31 10:17:16 +0800

    Leetcode Practice - Amazon #200 Number of Islands

--- src/main/java/leetcode/amazon/NumberOfIslands.java
+++ src/main/java/leetcode/amazon/NumberOfIslands.java
@@ -5,7 +5,7 @@ class NumberOfIslands {
         int numberOfIslandFound = 0;
         for (int row = 0; row < grid.length; row++) {
             for (int column = 0; column < grid[row].length; column++) {
-                // Create exception for starting point
+                // Create exception for starting coordinates
                 if (grid[row][column] == '1') {
                     numberOfIslandFound++;
                     grid = destroyFoundIsland(grid, row, column);
