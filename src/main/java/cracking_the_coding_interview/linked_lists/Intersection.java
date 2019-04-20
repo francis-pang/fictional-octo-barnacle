4ed8ee267d8be07c571878ba588fb46541856184
boyshawn <boyshawn@hotmail.com>
2019-04-20 11:16:16 +0800

    Data Structure Chapter 2 Linked Lists 2.7 Intersection Leetcode Practice - #160 Intersection of Two Linked Lists

---
+++ src/main/java/cracking_the_coding_interview/linked_lists/Intersection.java
@@ -0,0 +1,4 @@
+package cracking_the_coding_interview.linked_lists;
+/**
+ * Answer is in {@link leetcode.amazon.IntersectionOfTwoLinkedLists}
+ */
---
+++ src/main/java/leetcode/amazon/IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,38 @@
+package leetcode.amazon;
+
+import java.util.HashSet;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class IntersectionOfTwoLinkedLists {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    if (headA == null || headB == null) {
+      return null;
+    }
+    //Pre-computation
+    HashSet<ListNode> listASet = new HashSet<>();
+    ListNode iteratingNode = headA;
+    do {
+      listASet.add(iteratingNode);
+      iteratingNode = iteratingNode.next;
+    } while (iteratingNode != null);
+    //Computation
+    iteratingNode = headB;
+    do {
+      if (listASet.contains(iteratingNode)) {
+        return iteratingNode;
+      }
+      iteratingNode = iteratingNode.next;
+    } while (iteratingNode != null);
+    return null;
+  }
+}
