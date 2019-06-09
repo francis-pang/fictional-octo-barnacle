920adeaf5f6594d04459090ad9a7f07b46bdc4b9
Francis Pang <boyshawn@hotmail.com>
2019-06-10 02:43:06 +0800

    Data Structure Chapter 4 Trees and Graphs 4.8 First Common Ancestor Leetcode Practice - #236 Lowest Common Ancestor of a Binary Tree

---
+++ src/main/java/crackingthecodinginterview/treesandgraphs/FirstCommonAncestor.java
@@ -0,0 +1,11 @@
+package crackingthecodinginterview.treesandgraphs;
+
+/**
+ * Design an algorithm and write code to find the first common ancestor
+ * of two nodes in a binary tree. Avoid storing additional nodes in a data structure. NOTE: This is not
+ * necessarily a binary search tree.
+ * <p>
+ * Answer in {@link leetcode.LowestCommonAncestorOfABinaryTree}
+ */
+public class FirstCommonAncestor {
+}
