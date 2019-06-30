ab4aad2fc364b03bc92cd28263a3cd01ca5c2217
Francis Pang <boyshawn@hotmail.com>
2019-06-30 15:45:41 +0800

    Concepts and Algorithmse Chapter 8 Recursion and Dynamic Programming 8.11 Coins Leetcode Practice - #322 Coin Change

--- src/main/java/leetcode/amazon/CoinChange.java
+++ src/main/java/leetcode/amazon/CoinChange.java
@@ -1,42 +1,55 @@
 package leetcode.amazon;
 
-import java.util.Arrays;
 
 public class CoinChange {
-    int[] minimumCoinFromThisAmount;
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    int[] coins = {1, 2, 5};
+    System.out.println(solution.coinChange(coins, 10));
+  }
+
+  static class Solution {
+    private int[] smallestCoinsForAmount;
 
     public int coinChange(int[] coins, int amount) {
-        if (amount <= 0) {
-            return 0;
-        }
-        minimumCoinFromThisAmount = new int[amount + 1];
-        Arrays.fill(minimumCoinFromThisAmount, Integer.MAX_VALUE);
-        // So the correct method is to use Dynamic Programming
+      if (amount <= 0) {
+        return 0;
+      }
+      smallestCoinsForAmount = new int[amount + 1];
+      int leastCoinUsed = leastCoinUsed(coins, amount);
+      if (leastCoinUsed == Integer.MAX_VALUE) {
+        return -1;
+      } else {
+        return leastCoinUsed;
+      }
 
-        // We want to take the minimum of all the 3 options, and store a shortest path
-        if (minimumCoinFromThisAmount[amount] != Integer.MAX_VALUE) {
-            return minimumCoinFromThisAmount[amount];
         }
-        for (int coinDenomination : coins) {
-            // System.out.println("Computing remaining amount of " + amount + " after deducting from " + coinDenomination);
-            if (amount == coinDenomination) {
-                minimumCoinFromThisAmount[amount] = 1;
-                break;
-            } else if (amount - coinDenomination > 0) {
-                minimumCoinFromThisAmount[amount] = Math.min(minimumCoinFromThisAmount[amount], coinChange(coins, amount - coinDenomination) + 1);
-            }
+
+    private int leastCoinUsed(int[] coins, int amount) {
+      int leastNumberOfCoinForAmount = Integer.MAX_VALUE;
+      for (int coinDenomination : coins) {
+        int numberOfCoinsUsed = 0;
+        if (amount == coinDenomination) {
+          smallestCoinsForAmount[amount] = 1;
+          return 1;
         }
-//        minimumCoinFromThisAmount[amount] = (minimumCoinFromThisAmount[amount] == Integer.MAX_VALUE)
-//                ? -1
-//                : minimumCoinFromThisAmount[amount];
-        System.out.println("Minimum coins for amount of " + amount + " = " + minimumCoinFromThisAmount[amount]);
-        return minimumCoinFromThisAmount[amount];
+        int amountLessCoin = amount - coinDenomination;
+        if (amountLessCoin < 0) {
+          continue;
+        }
+
+        if (smallestCoinsForAmount[amountLessCoin] != 0) {
+          numberOfCoinsUsed = smallestCoinsForAmount[amountLessCoin];
+        } else {
+          numberOfCoinsUsed = leastCoinUsed(coins, amountLessCoin);
+        }
+        if (numberOfCoinsUsed != Integer.MAX_VALUE) {
+          numberOfCoinsUsed++;
+          leastNumberOfCoinForAmount = Math.min(leastNumberOfCoinForAmount, numberOfCoinsUsed);
+        }
+      }
+      smallestCoinsForAmount[amount] = leastNumberOfCoinForAmount;
+      return leastNumberOfCoinForAmount;
     }
-
-    public static void main(String[] args) {
-        int[] coins = {186, 419, 83, 408};
-        int amount = 6249;
-        CoinChange coinChange = new CoinChange();
-        System.out.println(coinChange.coinChange(coins, amount));
     }
 }
