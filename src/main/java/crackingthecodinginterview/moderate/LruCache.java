403a6895214f6ca91f673fb31ceb0732c7b401ff
Francis Pang <boyshawn@hotmail.com>
2019-07-13 15:10:31 +0800

    Additional Review Problems Chapter 16 Moderate 16.2S LRU Cache

---
+++ src/main/java/crackingthecodinginterview/moderate/LruCache.java
@@ -0,0 +1,10 @@
+package crackingthecodinginterview.moderate;
+
+/**
+ * Design and build a "least recently used" cache, which evicts the least recently used item. The cache should map
+ * from keys to values (allowing you to insert and retrieve a value associated with a particular key) and be
+ * initialized with a max size. When it is full, it should evict the least recently used item.
+ * Answer is in {@link leetcode.amazon.LRUCache}
+ */
+public class LruCache {
+}
