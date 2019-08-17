7634e8018364f5ce92f735ea7e6cdc5c837b8863
Francis Pang <boyshawn@hotmail.com>
2019-08-17 10:31:37 +0800

    Concepts and Algorithms Chapter 10 Sorting and Searching 10.9 Sorted Matrix Search

---
+++ src/main/java/crackingthecodinginterview/sortingandsearching/SortedMatrixSearch.java
@@ -0,0 +1,9 @@
+package crackingthecodinginterview.sortingandsearching;
+
+/**
+ * Given an M x N matrix in which each row and each column is sorted in ascending order, write a method to find an
+ * element.
+ * Answer is in {@link leetcode.SearchA2dMatrix2}
+ */
+public class SortedMatrixSearch {
+}
