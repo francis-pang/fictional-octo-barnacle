87b4709e205d7c218db30b99df4b4897f247a3a0
Francis Pang <boyshawn@hotmail.com>
2019-10-28 09:12:02 +0800

    Leetcode 482. License Key Formatting

--- src/main/java/leetcode/google/LicenseKeyFormatting.java
+++ src/main/java/leetcode/google/LicenseKeyFormatting.java
@@ -1,31 +1,18 @@
 package leetcode.google;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LicenseKeyFormatting {
   public String licenseKeyFormatting(String string, int k) {
-    List<Character> answer = new ArrayList<>();
+    StringBuilder stringBuilder = new StringBuilder();
     int stringLength = 0;
     for (int i = string.length() - 1; i >= 0; i--) {
       if (string.charAt(i) != '-') {
         if (stringLength > 0 && stringLength % k == 0) {
-          answer.add('-');
+          stringBuilder.append('-');
         }
-        answer.add(string.charAt(i));
+        stringBuilder.append(string.charAt(i));
         stringLength++;
       }
     }
-    return reverseString(answer);
-  }
-
-  private String reverseString(List<Character> answerReverse) {
-    StringBuilder stringBuilder = new StringBuilder();
-    for (int i = answerReverse.size() - 1; i >= 0; i--) {
-      char character = answerReverse.get(i);
-      character = Character.toUpperCase(character);
-      stringBuilder.append(character);
-    }
-    return stringBuilder.toString();
+    return stringBuilder.reverse().toString().toUpperCase();
   }
 }
