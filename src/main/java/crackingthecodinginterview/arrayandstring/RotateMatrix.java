857f6b1959d411473193f920549513c7771e0532
Francis Pang <boyshawn@hotmail.com>
2019-10-31 06:51:09 +0800

    Data Structure Chapter 1 I Arrays and Strings 1.7 Rotate Matrix
    This is an 2nd attempt at the question, as a mean of revision and practice coding on paper.
    commit add982 Copy straight from the code written on paper.
    commit 12f942 Add in main method and helper method to test code. Fix bugs found on 2 lines and rotate method, which is outside the coding scope
    commit 1ca621 Port the beautified code with comment to RotateMatrix class, update test case.

--- src/main/java/crackingthecodinginterview/arrayandstring/RotateMatrix.java
+++ src/main/java/crackingthecodinginterview/arrayandstring/RotateMatrix.java
@@ -1,37 +1,86 @@
 package crackingthecodinginterview.arrayandstring;
 
 public class RotateMatrix {
-  int matrixLength;
-  int[][] matrix;
-
-  public int[][] rotateTwoDimensionalMatrix(int[][] matrix) {
-    this.matrixLength = matrix.length;
-    if (matrixLength < 2) {
-      return matrix;
-    }
-    this.matrix = matrix;
-    int leftBound = 0;
-    int rightBound = matrixLength - 1;
-    do {
-      for (int i = leftBound; i < rightBound; i++) {
-        fourWayRotate(leftBound, i);
+  public void rotateImageLeft(int[][] image) {
+    int imageBoundary = image.length - 1;
+    // Start from the first row, we set the boundary where the swapping occurs. After each round, we reduce the
+    // boundary by 2 (first and last). This is because the length of the inner square has 2 less cell (one on each
+    // side). This applies the same for the column as well.
+    for (int row = 0; row < imageBoundary; row++) {
+      // Swap per column first, then go row by go
+      for (int column = row; column < imageBoundary; column++) {
+        // Determine the 4 coordinates to carry out the 4 layer swapping
+        Coordinate coordinate1 = new Coordinate(row, column);
+        Coordinate coordinate2 = getCopyDestination(image.length, coordinate1);
+        Coordinate coordinate3 = getCopyDestination(image.length, coordinate2);
+        Coordinate coordinate4 = getCopyDestination(image.length, coordinate3);
+        rotate(image, coordinate1, coordinate2, coordinate3, coordinate4);
       }
-      leftBound++;
-      rightBound--;
-    } while (leftBound < rightBound);
-    return this.matrix;
+      imageBoundary--;
+    }
   }
 
-  private void fourWayRotate(int x, int y) {
-    int a = x;
-    int b = y;
-    int temp = matrix[x][y];
-    for (int i = 0; i < 3; i++) {
-      int swap = this.matrixLength - a - 1;
-      matrix[a][b] = matrix[b][swap];
-      a = b;
-      b = swap;
+  /**
+   * Based on the origin coordinate, calculate the coordinate destination to copy the value in the matrix to
+   *
+   * @param matrixLength the length of the square matrix, needed for calculation
+   * @param coordinate   origin coordinate
+   * @return coordinate of copy destination
+   */
+  private Coordinate getCopyDestination(int matrixLength, Coordinate coordinate) {
+    int newColumn = coordinate.row;
+    int newRow = matrixLength - 1 - coordinate.column;
+    return new Coordinate(newRow, newColumn);
+  }
+
+  private void rotate(int[][] array,
+                      Coordinate coordinate1,
+                      Coordinate coordinate2,
+                      Coordinate coordinate3,
+                      Coordinate coordinate4) {
+    // Although this is a rotation algorithm, effectively we are just doing swapping in a merry-go-round fashion. We
+    // want to move coordinate 1 to 2, then 2 to 3, and 3 to 4. But the sequence is reversed so that the original
+    // value stored at each cells is preserved, then one temp value to store the value to be overwritten first.
+    int swapValue = array[coordinate4.row][coordinate4.column];
+    array[coordinate4.row][coordinate4.column] = array[coordinate3.row][coordinate3.column];
+    array[coordinate3.row][coordinate3.column] = array[coordinate2.row][coordinate2.column];
+    array[coordinate2.row][coordinate2.column] = array[coordinate1.row][coordinate1.column];
+    array[coordinate1.row][coordinate1.column] = swapValue;
+  }
+
+  public class Coordinate {
+    public int row;
+    public int column;
+
+    public Coordinate(int row, int column) {
+      this.row = row;
+      this.column = column;
     }
-    matrix[a][b] = temp;
+  }
+
+  public static void main(String[] args) {
+    int[][] inputArray = {
+        {11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20},
+        {21, 22, 23, 24, 25},
+        {26, 27, 28, 29, 30},
+        {31, 32, 33, 34, 35}
+    };
+    System.out.println("Input:");
+    printSquareArray(inputArray);
+    System.out.println();
+    RotateMatrix rotateMatrix = new RotateMatrix();
+    rotateMatrix.rotateImageLeft(inputArray);
+    System.out.println("Output:");
+    printSquareArray(inputArray);
+  }
+
+  public static void printSquareArray(int[][] array) {
+    for (int row = 0; row < array.length; row++) {
+      for (int column = 0; column < array.length; column++) {
+        System.out.print(array[row][column] + ", ");
+      }
+      System.out.print("\n");
+    }
   }
 }
--- src/test/java/crackingthecodinginterview/arrayandstring/RotateMatrixTest.java
+++ src/test/java/crackingthecodinginterview/arrayandstring/RotateMatrixTest.java
@@ -1,9 +1,9 @@
 package crackingthecodinginterview.arrayandstring;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class RotateMatrixTest {
   private static RotateMatrix rotateMatrix;
@@ -16,30 +16,76 @@ class RotateMatrixTest {
   @Test
   void testOneByOneMatrix() {
     int[][] oneByOneMatrix = {{4}};
-    assertTrue(Matrix.isTwoMatrixSame(oneByOneMatrix, rotateMatrix.rotateTwoDimensionalMatrix(oneByOneMatrix)));
+    int[][] expectedMatrix = {{4}};
+    rotateMatrix.rotateImageLeft(oneByOneMatrix);
+    assertArrayEquals(expectedMatrix, oneByOneMatrix);
   }
 
   @Test
   void testTwoByTwoMatrix() {
-    int[][] testMatrix = {{1, 2},
+    int[][] testMatrix = {
+        {1, 2},
         {3, 4}};
-    int[][] expectedMatrix = {{2, 4},
+    int[][] expectedMatrix = {
+        {2, 4},
         {1, 3}};
-    assertTrue(Matrix.isTwoMatrixSame(expectedMatrix, rotateMatrix.rotateTwoDimensionalMatrix(testMatrix)));
+    rotateMatrix.rotateImageLeft(testMatrix);
+    assertArrayEquals(expectedMatrix, testMatrix);
   }
 
   @Test
   void testFiveByFiveMatrix() {
-    int[][] testMatrix = {{96, 14, 62, 64, 36},
+    int[][] testMatrix = {
+        {96, 14, 62, 64, 36},
         {20, 54, 67, 25, 35},
         {94, 28, 81, 78, 74},
         {36, 64, 26, 16, 97},
-        {64, 76, 90, 21, 68}};
-    int[][] expectedMatrix = {{36, 35, 74, 97, 68},
+        {64, 76, 90, 21, 68}
+    };
+    int[][] expectedMatrix = {
+        {36, 35, 74, 97, 68},
         {64, 25, 78, 16, 21},
         {62, 67, 81, 26, 90},
         {14, 54, 28, 64, 76},
-        {96, 20, 94, 36, 64}};
-    assertTrue(Matrix.isTwoMatrixSame(expectedMatrix, rotateMatrix.rotateTwoDimensionalMatrix(testMatrix)));
+        {96, 20, 94, 36, 64}
+    };
+    rotateMatrix.rotateImageLeft(testMatrix);
+    assertArrayEquals(expectedMatrix, testMatrix);
+  }
+
+  @Test
+  void testThreeByThreeMatrix() {
+    int[][] testMatrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int[][] expectedMatrix = {
+        {3, 6, 9},
+        {2, 5, 8},
+        {1, 4, 7}
+    };
+    rotateMatrix.rotateImageLeft(testMatrix);
+    assertArrayEquals(expectedMatrix, testMatrix);
+  }
+
+  @Test
+  void testFiveByFiveMatrix2() {
+    int[][] testMatrix = {
+        {11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20},
+        {21, 22, 23, 24, 25},
+        {26, 27, 28, 29, 30},
+        {31, 32, 33, 34, 35}
+    };
+    int[][] expectedMatrix = {
+        {15, 20, 25, 30, 35},
+        {14, 19, 24, 29, 34},
+        {13, 18, 23, 28, 33},
+        {12, 17, 22, 27, 32},
+        {11, 16, 21, 26, 31}
+    };
+    rotateMatrix.rotateImageLeft(testMatrix);
+    assertArrayEquals(expectedMatrix, testMatrix);
   }
 }
