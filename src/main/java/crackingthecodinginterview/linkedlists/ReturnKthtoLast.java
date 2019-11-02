2d2dd79af4b08b78119673d932f0a10f6227c73b
Francis Pang <boyshawn@hotmail.com>
2019-11-02 20:54:23 +0800

    Data Structure Chapter 2 Linked Lists 2.2 Return Kth to Last
    This is 2nd attempt to the question

--- src/main/java/crackingthecodinginterview/linkedlists/ReturnKthtoLast.java
+++ src/main/java/crackingthecodinginterview/linkedlists/ReturnKthtoLast.java
@@ -1,21 +1,65 @@
 package crackingthecodinginterview.linkedlists;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.Objects;
+import java.util.StringJoiner;
 
 public class ReturnKthtoLast {
-  public Integer getKToLastElement(List<Integer> list, int k) {
-    int listSize = 0;
-    Iterator<Integer> iterator = list.iterator();
-    while (iterator.hasNext()) {
-      iterator.next();
-      listSize++;
-    }
-    iterator = list.iterator();
-    for (int i = 0; i < (listSize - k - 1); i++) {
-      iterator.next();
+  // This problem with this we do not know the ending of the linked list. If we know the end index, or the size of
+  // the linked list, we can just iterating through the linked list to that particular (size/end - k) index.
+  // The native solution is simply to get the size of the linked list through one iteration of the linked list. Then
+  // I traverse to the (size - k)th index. This will solve my problem. However, this will mean that I need to
+  // traverse through the linked list twice separately. So the strict time complexity is O(2n) at worse, if k = 1. I
+  // can solve this limitation by introducing a 2nd pointer. I will let the 1st pointer to run through k element
+  // before the 2nd pointer starts. So the 2nd pointer is always k steps behind the 1st pointer. When the 1st pointer
+  // reaches the end of the linked list, the 2nd pointer will reach the (size - k)th index. And I will just return
+  // that item. This will reduce the time complexity to O(n).
+  public Node getKToLastElement(Node root, int k) {
+    Node frontPointer = root;
+
+    // Run the 1st pointer for k time so that the 2nd pointer will be k steps behind
+    for (int i = 0; i < k; i++) {
+      if (frontPointer != null) {
+        frontPointer = frontPointer.next;
+      } else {
+        return null; // Return null if the size linked list is less than k
+      }
     }
 
-    return iterator.next();
+    Node backPointer = root;
+    while (frontPointer != null) {
+      frontPointer = frontPointer.next;
+      backPointer = backPointer.next;
+    }
+    return backPointer;
+  }
+
+  public static class Node {
+    public int value;
+    public Node next;
+
+    public Node(int value) {
+      this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (!(o instanceof Node)) return false;
+      Node node = (Node) o;
+      return value == node.value &&
+          Objects.equals(next, node.next);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(value, next);
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", Node.class.getSimpleName() + "[", "]")
+          .add("value=" + value)
+          .toString();
+    }
   }
 }
--- src/test/java/crackingthecodinginterview/linkedlists/ReturnKthtoLastTest.java
+++ src/test/java/crackingthecodinginterview/linkedlists/ReturnKthtoLastTest.java
@@ -3,43 +3,60 @@ package crackingthecodinginterview.linkedlists;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReturnKthtoLastTest {
   private static ReturnKthtoLast returnKthtoLast;
+  private static ReturnKthtoLast.Node node1;
+  private static ReturnKthtoLast.Node node2;
+  private static ReturnKthtoLast.Node node3;
+  private static ReturnKthtoLast.Node node4;
+  private static ReturnKthtoLast.Node node5;
+  private static ReturnKthtoLast.Node node6;
+  private static ReturnKthtoLast.Node node7;
+  private static ReturnKthtoLast.Node node8;
+  private static ReturnKthtoLast.Node node9;
+  private static ReturnKthtoLast.Node node10;
+  private static ReturnKthtoLast.Node node11;
 
   @BeforeAll
   static void setUpOnce() {
     returnKthtoLast = new ReturnKthtoLast();
+    node1 = new ReturnKthtoLast.Node(1);
+    node2 = new ReturnKthtoLast.Node(2);
+    node3 = new ReturnKthtoLast.Node(3);
+    node4 = new ReturnKthtoLast.Node(4);
+    node5 = new ReturnKthtoLast.Node(5);
+    node6 = new ReturnKthtoLast.Node(6);
+    node7 = new ReturnKthtoLast.Node(7);
+    node8 = new ReturnKthtoLast.Node(8);
+    node9 = new ReturnKthtoLast.Node(9);
+    node10 = new ReturnKthtoLast.Node(10);
+    node11 = new ReturnKthtoLast.Node(11);
+    node1.next = node2;
+    node2.next = node3;
+    node3.next = node4;
+    node4.next = node5;
+    node5.next = node6;
+    node6.next = node7;
+    node7.next = node8;
+    node8.next = node9;
+    node9.next = node10;
+    node10.next = node11;
   }
 
   @Test
   void testNormalCase() {
-    List<Integer> testList = new ArrayList<>();
-    for (int i = 1; i < 11; i++) {
-      testList.add(i);
-    }
-    assertEquals(Integer.valueOf(7), returnKthtoLast.getKToLastElement(testList, 3));
+    assertEquals(node9, returnKthtoLast.getKToLastElement(node1, 3));
   }
 
   @Test
   void testFirstIsLastKCase() {
-    List<Integer> testList = new ArrayList<>();
-    for (int i = 4; i > 0; i--) {
-      testList.add(i);
-    }
-    assertEquals(Integer.valueOf(4), returnKthtoLast.getKToLastElement(testList, 4));
+    assertEquals(node1, returnKthtoLast.getKToLastElement(node1, 11));
   }
 
   @Test
   void testLastCase() {
-    List<Integer> testList = new ArrayList<>();
-    for (int i = 0; i < 5; i++) {
-      testList.add(i * 6);
-    }
-    assertEquals(Integer.valueOf(24), returnKthtoLast.getKToLastElement(testList, 0));
+    assertEquals(node11, returnKthtoLast.getKToLastElement(node1, 1));
   }
 }
