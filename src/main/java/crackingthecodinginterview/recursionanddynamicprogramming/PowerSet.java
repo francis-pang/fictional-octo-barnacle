bf63d50cd8e999b7056d5e71a92073b9a2538ea0
Francis Pang <boyshawn@hotmail.com>
2019-11-05 04:26:02 +0800

    Concepts and Algorithms Chapter 8 Recursion and Dynamic Programming 8.4 Power Set
    2nd attempt of the question

--- src/main/java/crackingthecodinginterview/recursionanddynamicprogramming/PowerSet.java
+++ src/main/java/crackingthecodinginterview/recursionanddynamicprogramming/PowerSet.java
@@ -1,8 +1,27 @@
 package crackingthecodinginterview.recursionanddynamicprogramming;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Write a method to return all subsets of a set.
  * Answer is in {@link leetcode.Subsets}
  */
 public class PowerSet {
+  public Set<Set<Integer>> powerSet(Set<Integer> set) {
+    if (set.isEmpty()) {
+      return new HashSet<>();
+    }
+    final Set<Set<Integer>> combinedSet = new HashSet<>();
+    final Iterator<Integer> iterator = set.iterator();
+    final int element = iterator.next();
+    set.remove(element);
+    Set<Set<Integer>> spiltSet = powerSet(set);
+    for (Set<Integer> smallerSet : spiltSet) {
+      smallerSet.add(element);
+      combinedSet.add(smallerSet);
+    }
+    return combinedSet;
+  }
 }
