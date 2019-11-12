077efcfa2dcf9602bdef9ff39341dc8119ac6633
Francis Pang <boyshawn@hotmail.com>
2019-11-13 00:16:03 +0800

    APAC-ABCS19: Graph Theory  Snakes and Ladders: The Quickest Way Up
    Incldue a BFS and DP solution. DP solution is still not working.

--- src/main/java/facebook/abcs/graph/SnakesAndLaddersTheQuickestWayUp.java
+++ src/main/java/facebook/abcs/graph/SnakesAndLaddersTheQuickestWayUp.java
@@ -9,7 +9,66 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class SnakesAndLaddersTheQuickestWayUp {
-  static int quickestWayUp(int[][] ladders, int[][] snakes) {
+  static int quickestWayUpDP(int[][] ladders, int[][] snakes) {
+    // This will be method which we use bottom-up iterative dynamic programming to get the minimum steps
+    int[] minimumSteps = new int[101];
+    for (int i = 0; i < minimumSteps.length; i++) {
+      minimumSteps[i] = Integer.MAX_VALUE;
+    }
+    minimumSteps[1] = 0;
+
+    int[] ladderEndPoints = convertLadderOrSnakeToOneDimensionArray(ladders);
+    int[] snakeEndPoints = convertLadderOrSnakeToOneDimensionArray(snakes);
+
+    for (int currentGridPos = 1; currentGridPos < 100; currentGridPos++) {
+      // So this means that there is a snake at the current grid, and we need to add the number of steps by the
+      // number of steps at the tails of the snake (end points).
+      int minimumStepFromCurrentGridPos = minimumSteps[currentGridPos];
+      int snakeEndPoint = snakeEndPoints[currentGridPos];
+      if (snakeEndPoint != 0) {
+        // Taken this step from https://www.hackerrank.com/challenges/the-quickest-way-up/forum/comments/91508
+        if (minimumSteps[snakeEndPoint] < minimumStepFromCurrentGridPos) {
+          minimumSteps[snakeEndPoint] = minimumStepFromCurrentGridPos;
+          currentGridPos = snakeEndPoint;
+        }
+      }
+      int nextMinimumStep = minimumStepFromCurrentGridPos + 1;
+      for (int dice = 1; dice <= 6; dice++) {
+        int destinationGrid = currentGridPos + dice;
+        if (destinationGrid > 100) {
+          continue;
+        }
+        if (minimumSteps[destinationGrid] == 0 || nextMinimumStep < minimumSteps[destinationGrid]) {
+          minimumSteps[destinationGrid] = nextMinimumStep;
+        }
+        if (ladderEndPoints[destinationGrid] != 0) {
+          int ladderEndPoint = ladderEndPoints[destinationGrid];
+          if (nextMinimumStep < minimumSteps[ladderEndPoint]) {
+            minimumSteps[ladderEndPoint] = nextMinimumStep;
+          }
+        }
+      }
+    }
+    if (minimumSteps[100] == 0) {
+      return -1;
+    } else {
+      return minimumSteps[100];
+    }
+  }
+
+  private static int[] convertLadderOrSnakeToOneDimensionArray(int[][] journeyPoints) {
+    int[] endPoints = new int[101];
+    Arrays.stream(journeyPoints)
+        .forEach(journey -> {
+          int start = journey[0];
+          int end = journey[1];
+          endPoints[start] = end;
+        });
+    return endPoints;
+  }
+
+  static int quickestWayUpBfs(int[][] ladders, int[][] snakes) {
+    // This will be the method which we use BFS to build the graph and get the minimum steps
     int numberOfStep = 0;
     boolean[] processGrid = new boolean[101];
     Queue<Integer> unprocessedGrids;
@@ -61,34 +120,21 @@ public class SnakesAndLaddersTheQuickestWayUp {
   }
 
   public static void main(String[] args) throws FileNotFoundException {
-    final Scanner scanner = new Scanner("2\n" +
-        "3\n" +
-        "32 62\n" +
-        "42 68\n" +
-        "12 98\n" +
-        "7\n" +
-        "95 13\n" +
-        "97 25\n" +
-        "93 37\n" +
-        "79 27\n" +
-        "75 19\n" +
-        "49 47\n" +
-        "67 17\n" +
-        "4\n" +
-        "8 52\n" +
-        "6 80\n" +
-        "26 42\n" +
-        "2 72\n" +
-        "9\n" +
-        "51 19\n" +
-        "39 11\n" +
-        "37 29\n" +
-        "81 3\n" +
-        "59 5\n" +
-        "79 23\n" +
-        "53 7\n" +
-        "43 33\n" +
-        "77 21 ");
+    final Scanner scanner = new Scanner("3\n" +
+        "2\n" +
+        "3 54\n" +
+        "37 100\n" +
+        "1\n" +
+        "56 33\n" +
+        "2\n" +
+        "3 57\n" +
+        "8 100\n" +
+        "1\n" +
+        "88 44\n" +
+        "1\n" +
+        "7 98\n" +
+        "1\n" +
+        "99 1");
     int t = scanner.nextInt();
     scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
@@ -123,7 +169,7 @@ public class SnakesAndLaddersTheQuickestWayUp {
         }
       }
 
-      int result = quickestWayUp(ladders, snakes);
+      int result = quickestWayUpDP(ladders, snakes);
       System.out.println(result);
     }
   }
