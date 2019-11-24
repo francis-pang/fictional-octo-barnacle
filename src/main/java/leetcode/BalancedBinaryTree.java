71116eb24c28a6398aaeccc6888b25c11b075106
Francis Pang <boyshawn@hotmail.com>
2019-11-24 13:40:52 +0800

    Leetcode 110. Balanced Binary Tree - 2nd attempt

--- src/main/java/leetcode/BalancedBinaryTree.java
+++ src/main/java/leetcode/BalancedBinaryTree.java
@@ -1,50 +1,35 @@
 package leetcode;
 
 public class BalancedBinaryTree {
-  /**
-   * Definition for a binary tree node.
-   * public class TreeNode {
-   * int val;
-   * TreeNode left;
-   * TreeNode right;
-   * TreeNode(int x) { val = x; }
-   * }
-   */
-  class Solution {
-    public boolean isBalanced(TreeNode root) {
-      return (calculateHeight(root) != -2);
+  private static int NOT_BALANCED = -1;
+
+  public boolean isBalanced(TreeNode root) {
+    return calculateHeight(root) != NOT_BALANCED;
+  }
+
+  private int calculateHeight(TreeNode root) {
+    if (root == null) {
+      return 0;
     }
-
-    public int calculateHeight(TreeNode node) {
-      if (node == null) {
-        return -1;
-      }
-
-      int leftSubTreeHeight = calculateHeight(node.left);
-      if (leftSubTreeHeight == -2) {
-        return -2;
-      }
-
-      int rightSubTreeHeight = calculateHeight(node.right);
-      if (rightSubTreeHeight == -2) {
-        return -2;
-      }
-
-      if (Math.abs(leftSubTreeHeight - rightSubTreeHeight) > 1) {
-        return -2;
-      }
-
-      return Math.max(leftSubTreeHeight, rightSubTreeHeight) + 1;
+    int leftChildHeight = calculateHeight(root.left);
+    int rightChildHeight = calculateHeight(root.right);
+    if (leftChildHeight == NOT_BALANCED || rightChildHeight == NOT_BALANCED) {
+      return NOT_BALANCED;
     }
+    int heightDifference = leftChildHeight - rightChildHeight;
+    if (Math.abs(heightDifference) > 1) {
+      return NOT_BALANCED;
+    }
+    return Math.max(leftChildHeight, rightChildHeight) + 1;
+  }
 
-    class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
+  public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
-      TreeNode(int x) {
-        val = x;
-      }
+    TreeNode(int x) {
+      val = x;
     }
   }
 }
