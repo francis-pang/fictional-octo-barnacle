b7def8dcd1b71339a41d05747fe41af581ea3767
Francis Pang <boyshawn@hotmail.com>
2020-02-05 09:37:51 +0800

    Data Structure Chapter 2 Linked Lists 2.5 Sum Lists
    ae9e060de7a7b4f97637360a6761d6811d0cbb34 Fix method name to be same as Leetcode
    e4d8f8627c8b31ed0248320fffc7af5f12c77d5d Remove leetcode class answer to so that there isn't confusion of more optimised answers
    06843daac74a61da60df8e473a96a60157dd5bf3 Implement the follow up question with reverse link list method

--- src/main/java/crackingthecodinginterview/linkedlists/SumLists.java
+++ src/main/java/crackingthecodinginterview/linkedlists/SumLists.java
@@ -1,27 +1,107 @@
 package crackingthecodinginterview.linkedlists;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.StringJoiner;
 
 public class SumLists {
-  public List<Integer> addTwoNumbersRepresentByLinkedLists(List<Integer> numberOne, List<Integer> numberTwo) {
-    Iterator<Integer> numberOneIterator = numberOne.iterator();
-    Iterator<Integer> numberTwoIterator = numberTwo.iterator();
-    List<Integer> sumList = new ArrayList<>();
-    while (numberOneIterator.hasNext() && numberOneIterator.hasNext()) {
-      Integer digitOne = numberOneIterator.next();
-      Integer digitTwo = numberTwoIterator.next();
-      sumList.add(digitOne + digitTwo);
+  public ListNode addTwoNumbers(ListNode head1, ListNode head2) {
+    ListNode ansHead = new ListNode(0);
+    boolean carry = false;
+    ListNode ansListNode = ansHead;
+    ListNode listNode1 = head1;
+    ListNode listNode2 = head2;
+    while (listNode1 != null || listNode2 != null) {
+      int sum = 0;
+      if (carry) {
+        sum += 1;
+        carry = false;
+      }
+      if (listNode1 != null && listNode2 != null) {
+        sum += listNode1.val + listNode2.val;
+      } else if (listNode1 != null) {
+        sum += listNode1.val;
+      } else if (listNode2 != null) {
+        sum += listNode2.val;
+      }
+      if (sum >= 10) {
+        sum = sum % 10;
+        carry = true;
+      }
+      ListNode ans = new ListNode(sum);
+      ansListNode.next = ans;
+      ansListNode = ans;
+      if (listNode1 != null) {
+        listNode1 = listNode1.next;
+      }
+      if (listNode2 != null) {
+        listNode2 = listNode2.next;
+      }
     }
-    while (numberOneIterator.hasNext()) {
-      Integer digitOne = numberOneIterator.next();
-      sumList.add(digitOne);
+    if (carry) {
+      ansListNode.next = new ListNode(1);
     }
-    while (numberTwoIterator.hasNext()) {
-      Integer digitTwo = numberTwoIterator.next();
-      sumList.add(digitTwo);
+    return ansHead.next;
+  }
+
+  public ListNode addForwardReadingNumber(ListNode head1, ListNode head2) {
+    ListNode reverseHead1 = reverseLinkedList(head1);
+    ListNode reverseHead2 = reverseLinkedList(head2);
+    ListNode reverseAnswer = addTwoNumbers(reverseHead1, reverseHead2);
+    return reverseLinkedList(reverseAnswer);
+  }
+
+  private ListNode reverseLinkedList(ListNode head) {
+    ListNode prev = null;
+    ListNode next;
+    ListNode node = head;
+    while (node != null) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
     }
-    return sumList;
+    return prev;
+  }
+
+  public static class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+      this.val = val;
+    }
+
+    @Override
+    public String toString() {
+      return new StringJoiner(", ", ListNode.class.getSimpleName() + "[", "]")
+          .add("val=" + val)
+          .toString();
+    }
+  }
+
+  public void printLinkedList(ListNode listNode) {
+    while (listNode != null) {
+      System.out.printf("%s, ", listNode.val);
+      listNode = listNode.next;
+    }
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    ListNode listNode11 = new ListNode(7);
+    ListNode listNode12 = new ListNode(1);
+    ListNode listNode13 = new ListNode(9);
+    listNode11.next = listNode12;
+    listNode12.next = listNode13;
+
+    ListNode listNode21 = new ListNode(5);
+    ListNode listNode22 = new ListNode(9);
+    listNode21.next = listNode22;
+
+    SumLists sumLists = new SumLists();
+    ListNode ans = sumLists.addTwoNumbers(listNode11, listNode21);
+    sumLists.printLinkedList(ans);
+
+    ListNode reversedLinkedList = sumLists.addForwardReadingNumber(listNode11, listNode21);
+    sumLists.printLinkedList(reversedLinkedList);
   }
 }
