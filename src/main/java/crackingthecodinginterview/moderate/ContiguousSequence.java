cfed283604fdfef7d2dd84cb53630bc060baab6a
Francis Pang <boyshawn@hotmail.com>
2020-02-06 09:58:26 +0800

    Additional Question Chapter 16 Moderate 16.17 Contiguous Sequence
    commit 714f47c7dcc804f6c0812aa6ff0011b3ff02b19e Copied from paper with minor typo correction, no logic error
    commit 1e23f057d583000487ec04f74212fa7b6a3bcfbd Rename for more readable variable name
    commit 4e4d8361ff3f51690407e39182337a79582d7e04 Simplify the if-else conditional branches

--- src/main/java/crackingthecodinginterview/moderate/ContiguousSequence.java
+++ src/main/java/crackingthecodinginterview/moderate/ContiguousSequence.java
@@ -1,22 +1,77 @@
 package crackingthecodinginterview.moderate;
 
 public class ContiguousSequence {
-  public int biggestSumOf(int[] integers) {
-    if (integers == null || integers.length == 0) {
+  public int biggestSum(int[] array) {
+    int endP = condense(array);
+    int biggest = Integer.MIN_VALUE;
+    int pos = -1;
+    while (biggest <= 0 && pos <= endP) {
+      pos++;
+      if (biggest < array[pos]) {
+        biggest = array[pos];
+      }
+    }
+    boolean usedPrevious = true;
+    while ((pos + 2) <= endP) {
+      int sum = Integer.MIN_VALUE;
+      if (usedPrevious) {
+        sum = biggest + array[pos + 1] + array[pos + 2];
+      }
+      int nextPos = array[pos + 2];
+      int big = max(biggest, sum, nextPos);
+      if (big == biggest) {
+        usedPrevious = false;
+      } else {
+        usedPrevious = true;
+        biggest = big;
+      }
+      pos += 2;
+    }
+    return biggest;
+  }
+
+  private int condense(int[] array) {
+    int endP = 0;
+    int accmulator = 0;
+    int start = 0;
+    for (; start < array.length; start++) {
+      if (array[start] != 0) {
+        accmulator = array[start];
+        break;
+      }
+    }
+    if (accmulator == 0) {
       return 0;
     }
-    int[] biggestSumArray = new int[integers.length];
-
-    for (int index = 0; index < integers.length; index++) {
-      if (index == 0) {
-        biggestSumArray[index] = integers[index];
+    for (int i = start + 1; i < array.length; i++) {
+      if (array[i] == 0) {
+        continue;
       }
-      if (integers[index] < biggestSumArray[index - 1]) {
-        biggestSumArray[index] = biggestSumArray[index - 1] + integers[index];
+      if ((accmulator < 0 && array[i] < 0) ||
+          (accmulator > 0 && array[i] > 0)) {
+        accmulator += array[i];
       } else {
-        biggestSumArray[index] = integers[index];
+        array[endP++] = accmulator;
+        accmulator = array[i];
       }
     }
-    return biggestSumArray[integers.length];
+    array[endP] = accmulator;
+    return endP;
+  }
+
+  private int max(int num1, int num2, int num3) {
+    return Math.max(Math.max(num1, num2), num3);
+  }
+
+  public static void main(String[] args) {
+    ContiguousSequence contiguousSequence = new ContiguousSequence();
+    int answer = contiguousSequence.biggestSum(new int[]{0, 2, -8, -1, 3, -2, -3, 4, 5, 10});
+    System.out.println(answer);
+    answer = contiguousSequence.biggestSum(new int[]{0, 0, 0, 0});
+    System.out.println(answer);
+    answer = contiguousSequence.biggestSum(new int[]{-5, -4, -5, -4});
+    System.out.println(answer);
+    answer = contiguousSequence.biggestSum(new int[]{2, -8, 5, -3, 6, -7, 5, -5, 0});
+    System.out.println(answer);
   }
 }
