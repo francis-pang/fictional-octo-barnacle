43edd66f00cf62428aea023c9fb37e098fbc4327
Francis Pang <boyshawn@hotmail.com>
2020-02-09 22:09:10 +0800

    Additional review question Chapter 17 Hard 17.9 Kth Multiple - Reattempt but not most optimised

--- src/main/java/crackingthecodinginterview/hard/KthMultiple.java
+++ src/main/java/crackingthecodinginterview/hard/KthMultiple.java
@@ -2,7 +2,11 @@ package crackingthecodinginterview.hard;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 /**
  * Design an algorithm to find the kth number such that the only prime factors are 3, 5, and 7. Note that 3, 5, and 7
@@ -10,42 +14,40 @@ import java.util.List;
  * multiples would be (in order) 1, 3, 5,7,9, 15, 21.
  */
 public class KthMultiple {
-  private static final int FIRST_NUMBER = 1;
-  private static final int ERROR_NUMBER = 0;
-  private List<Integer> multiples;
-
-  public int kNumber(List<Integer> primeFactors, int k) {
-    Collections.sort(primeFactors);
-    multiples = new ArrayList<>();
-    int[] multiples = new int[Math.max(k, primeFactors.size() + 1)];
-
-    // First number
-    if (k == 1) {
-      return FIRST_NUMBER;
+  public int getKthMultiple(int[] primes, int k) {
+    PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(k, (o1, o2) -> (o1 - o2) * -1);
+    Set<Integer> uniqueStore = new HashSet<>();
+    uniqueStore.add(1);
+    priorityQueue.add(1);
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    int listIterator = 0;
+    while (listIterator < k) {
+      int element = list.get(listIterator);
+      for (int prime : primes) {
+        int multiple = prime * element;
+        if (uniqueStore.contains(multiple)) {
+          continue;
+        }
+        if (priorityQueue.size() < k) {
+          list.add(multiple);
+          priorityQueue.add(multiple);
+          uniqueStore.add(multiple);
+        } else if (priorityQueue.peek() > multiple) {
+          uniqueStore.remove(priorityQueue.poll());
+          priorityQueue.add(multiple);
+          list.add(multiple);
+          uniqueStore.add(multiple);
+        }
+      }
+      listIterator++;
     }
-    multiples[0] = 1;
-
-    // Initialise first set of prime
-    for (int i = 0; i < primeFactors.size(); i++) {
-      multiples[1 + i] = primeFactors.get(i);
-    }
-
-    if (primeFactors.isEmpty()) {
-      return ERROR_NUMBER;
-    }
-
-    return getKMultiple(multiples, k, primeFactors);
+    return priorityQueue.poll();
   }
 
-  private int getKMultiple(int[] array, int index, List<Integer> primeFactors) {
-    if (index <= primeFactors.size() + 1) {
-      return array[index - 1];
-    }
-    int position = index / primeFactors.size();
-    if (array[position] == 0) {
-      array[position] = getKMultiple(array, position, primeFactors);
-    }
-    int reminder = (index - 2) % primeFactors.size();
-    return array[position] * primeFactors.get(reminder);
+  public static void main(String[] args) {
+    KthMultiple kthMultiple = new KthMultiple();
+    int[] primes = new int[]{3,5,7};
+    System.out.println(kthMultiple.getKthMultiple(primes, 11));
   }
 }
