71890472e7bd54f15d10dc7a905e0fb73ab3dcfa
Francis Pang <boyshawn@hotmail.com>
2020-02-09 19:22:25 +0800

    Data Structure Chapter 2 Linked Lists 2.8 Loop Detection - Reattempt

--- src/main/java/crackingthecodinginterview/linkedlists/LoopDetection.java
+++ src/main/java/crackingthecodinginterview/linkedlists/LoopDetection.java
@@ -1,28 +1,19 @@
 package crackingthecodinginterview.linkedlists;
 
 public class LoopDetection {
-  public static ListNode locateCycleSource(ListNode head) throws InterruptedException {
-    if (head == null) {
-      return null;
+  public static ListNode locateCycleSource(ListNode head) {
+    ListNode fastRunner = head;
+    ListNode slowRunner = head;
+    do {
+      slowRunner = slowRunner.next;
+      fastRunner = fastRunner.next.next;
+    } while (!slowRunner.equals(fastRunner));
+    slowRunner = head;
+    while (!fastRunner.equals(slowRunner)) {
+      slowRunner = slowRunner.next;
+      fastRunner = fastRunner.next;
     }
-    ListNode fastRunnerNode = head.next.next;
-    ListNode slowRunnerNode = head.next;
-    int counter = 0;
-    while (fastRunnerNode != slowRunnerNode) {
-      System.out.println("Inside " + counter++ + " iteration. Fast Runner is at " + fastRunnerNode.value + "; Slow "
-          + "runner is at " + slowRunnerNode.value);
-      fastRunnerNode = fastRunnerNode.next.next;
-      slowRunnerNode = slowRunnerNode.next;
-    }
-    System.out.println("Collision point is at " + fastRunnerNode.value);
-    slowRunnerNode = head;
-    while (fastRunnerNode != slowRunnerNode) {
-      System.out.println("Inside. Fast Runner is at " + fastRunnerNode.value + "; Slow "
-          + "runner is at " + slowRunnerNode.value);
-      fastRunnerNode = fastRunnerNode.next;
-      slowRunnerNode = slowRunnerNode.next;
-    }
-    return fastRunnerNode;
+    return slowRunner;
   }
 
   static class ListNode {
@@ -36,25 +27,19 @@ public class LoopDetection {
 
   public static void main(String[] args) {
     ListNode node1 = new ListNode(1);
-
     ListNode node2 = new ListNode(2);
     node1.next = node2;
-
     ListNode node3 = new ListNode(3);
     node2.next = node3;
-
     ListNode node4 = new ListNode(4);
     node3.next = node4;
-
     ListNode node5 = new ListNode(5);
     node4.next = node5;
+//    node5.next = node2;
+    ListNode node6 = new ListNode(6);
+    node5.next = node6;
+    node6.next = node1;
 
-    node5.next = node2;
-
-    try {
-      System.out.println(locateCycleSource(node1).value);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
+    System.out.println(locateCycleSource(node1).value);
   }
 }
