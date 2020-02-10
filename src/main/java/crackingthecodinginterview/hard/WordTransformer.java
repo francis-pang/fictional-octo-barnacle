a236d1f502dc26ed4721c933e842cce233373997
Francis Pang <boyshawn@hotmail.com>
2020-02-10 12:23:40 +0800

    Additional Review Questions Chapter 17 Hard 17.22 Word Transformer - Use DFS to solve the problem

--- src/main/java/crackingthecodinginterview/hard/WordTransformer.java
+++ src/main/java/crackingthecodinginterview/hard/WordTransformer.java
@@ -1,144 +1,117 @@
 package crackingthecodinginterview.hard;
 
-import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 
 public class WordTransformer {
-  private Map<String, LinkedList<String>> stringTransformationPathMap;
-
-  public List<String> findShortestPathToTransformWord(List<String> dictionary, String start, String target) {
-    Graph graph = constructGraph(dictionary);
-    BfsDirectionContext forwardTraversalBfs = new BfsDirectionContext(graph.nodes.get(start));
-    BfsDirectionContext backwardTraversalBfs = new BfsDirectionContext(graph.nodes.get(target));
-    forwardTraversalBfs.setReverseVisitedNodes(backwardTraversalBfs.visitedNodes);
-    backwardTraversalBfs.setReverseVisitedNodes(forwardTraversalBfs.visitedNodes);
-
-    Node intersectingNode = null;
-    while (!forwardTraversalBfs.nextLevelQueue.isEmpty() && !backwardTraversalBfs.nextLevelQueue.isEmpty()) {
-      intersectingNode = findIntersectingNode(forwardTraversalBfs);
-      if (intersectingNode != null) {
-        break;
+  public List<String> transformWord(List<String> dictionary, String start, String target) {
+    // Assume that both start and target words are in dictionary.
+    Map<Integer, Set<Node>> lengthToNodeSetTable = new HashMap<>();
+    // Prepare node for the graph
+    for (String word : dictionary) {
+      int wordLength = word.length();
+      if (!lengthToNodeSetTable.containsKey(wordLength)) {
+        lengthToNodeSetTable.put(wordLength, new HashSet<>());
       }
-      intersectingNode = findIntersectingNode(backwardTraversalBfs);
-      if (intersectingNode != null) {
-        break;
-      }
+      Set<Node> nodeSet = lengthToNodeSetTable.get(wordLength);
+      Node node = new Node(word);
+      nodeSet.add(node);
     }
-    if (intersectingNode == null) {
-      throw new IllegalStateException("Cannot find a path");
-    }
-    LinkedList<String> path = new LinkedList<>();
-    String word = intersectingNode.val;
-    while (word != null) {
-      path.addFirst(word);
-      word = forwardTraversalBfs.predecessorTable.get(word);
-    }
-    word = backwardTraversalBfs.predecessorTable.get(intersectingNode.val);
-    while (word != null) {
-      path.addLast(word);
-      word = backwardTraversalBfs.predecessorTable.get(word);
-    }
-    return path;
-  }
-
-  private Node findIntersectingNode(BfsDirectionContext bfsDirectionContext) {
-    Queue<Node> queue = bfsDirectionContext.nextLevelQueue;
-    bfsDirectionContext.nextLevelQueue = new ArrayDeque<>();
-    while (!queue.isEmpty()) {
-      Node node = queue.poll();
-      if (!bfsDirectionContext.visitedNodes.add(node)) {
-        continue;
-      }
-      for (Node neighbour : node.neighbours) {
-        if (!bfsDirectionContext.predecessorTable.containsKey(neighbour.val)) {
-          bfsDirectionContext.predecessorTable.put(neighbour.val, node.val);
+    // Build the edge for each node
+    Node startNode = null;
+    Node targetNode = null;
+    for (Set<Node> nodesOfSameLength : lengthToNodeSetTable.values()) {
+      for (Node node : nodesOfSameLength) {
+        String word = node.value;
+        if (word.equals(start)) {
+          startNode = node;
         }
-        if (bfsDirectionContext.reverseVisitedNodes.contains(neighbour)) {
-          return neighbour;
+        if (word.equals(target)) {
+          targetNode = node;
         }
-        bfsDirectionContext.nextLevelQueue.add(neighbour);
-      }
-    }
-    return null;
-  }
-
-  class BfsDirectionContext {
-    private Map<String, String> predecessorTable;
-    private Set<Node> visitedNodes;
-    private Queue<Node> nextLevelQueue;
-    private Set<Node> reverseVisitedNodes;
-
-    public BfsDirectionContext(Node startingNode) {
-      predecessorTable = new HashMap<>();
-      predecessorTable.put(startingNode.val, null);
-      visitedNodes = new HashSet<>();
-      nextLevelQueue = new ArrayDeque<>();
-      nextLevelQueue.add(startingNode);
-    }
-
-    public void setReverseVisitedNodes(Set<Node> reverseVisitedNodes) {
-      this.reverseVisitedNodes = reverseVisitedNodes;
-    }
-  }
-
-  private Graph constructGraph(List<String> dictionary) {
-    Graph graph = new Graph();
-    dictionary.forEach(word -> graph.nodes.put(word, new Node(word)));
-    dictionary.forEach(word -> {
-      Node node = graph.nodes.get(word);
-      // This is with the assumption that the word is limited to lower case alphabet.
-      for (int pos = 0; pos < word.length(); pos++) {
-        for (int i = 'a'; i <= 'z'; i++) {
-          String alteredWord = word.substring(0, pos) + Character.toString(i) + word.substring(pos + 1);
-          if (alteredWord.equals(word)) {
+        for (Node iteratingNode : nodesOfSameLength) {
+          if (iteratingNode.value.equals(word) || node.neighbours.contains(word)) {
             continue;
           }
-          Node neighbour = graph.nodes.get(alteredWord);
-          if (neighbour == null) {
-            continue;
+          String iteratingWord = iteratingNode.value;
+          if (hasOneEditDistance(word, iteratingWord)) {
+            iteratingNode.neighbours.add(node);
+            node.neighbours.add(iteratingNode);
           }
-          node.neighbours.add(neighbour);
-          neighbour.neighbours.add(node);
         }
       }
-    });
-    return graph;
-  }
-
-  public void printList(List<String> list) {
-    list.forEach(word -> System.out.printf("%s, ", word));
-    System.out.println();
-  }
-
-  class Graph {
-    public Map<String, Node> nodes;
-
-    public Graph() {
-      nodes = new HashMap<>();
     }
+    if (startNode == null || targetNode == null) {
+      return new ArrayList<>();
+    }
+    List<String> transformationPath = new ArrayList<>();
+    Set<Node> graph = lengthToNodeSetTable.get(start.length());
+    Set<Node> visited = new HashSet<>();
+    if (findTransformationPath(graph, visited, startNode, targetNode, transformationPath)) {
+      return transformationPath;
+    } else {
+      return new ArrayList<>();
+    }
+  }
+
+  private boolean findTransformationPath(Set<Node> graph, Set<Node> visitedNodes, Node node, Node targetNode, List<String> transformationPath) {
+    if (node.equals(targetNode)) {
+      transformationPath.add(targetNode.value);
+      return true;
+    }
+    if (!visitedNodes.add(node)) {
+      return false;
+    }
+    transformationPath.add(node.value);
+    for (Node neighbour : node.neighbours) {
+      if (findTransformationPath(graph, visitedNodes, neighbour, targetNode, transformationPath)) {
+        return true;
+      }
+    }
+    transformationPath.remove(transformationPath.size() - 1);
+    return false;
+  }
+
+  private boolean hasOneEditDistance(String word, String iteratingWord) {
+    boolean alreadyFoundOneEdit = false;
+    for (int i = 0; i < word.length(); i++) {
+      char character = word.charAt(i);
+      char iteratingCharacter = iteratingWord.charAt(i);
+      if (character != iteratingCharacter) {
+        if (!alreadyFoundOneEdit) {
+          alreadyFoundOneEdit = true;
+        } else {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
   class Node {
-    public String val;
-    public Set<Node> neighbours;
+    String value;
+    Set<Node> neighbours;
 
-    public Node(String val) {
-      this.val = val;
+    public Node(String value) {
+      this.value = value;
       neighbours = new HashSet<>();
     }
   }
 
   public static void main(String[] args) {
     WordTransformer wordTransformer = new WordTransformer();
-    List<String> dictionary = List.of("damp", "lamp", "limp", "lime", "like", "nut", "word", "transformer", "given", "two",
-        "of", "equal", "length", "that", "are", "in", "a");
-    List<String> result = wordTransformer.findShortestPathToTransformWord(dictionary, "damp", "like");
-    wordTransformer.printList(result);
+    List<String> dictionary;
+    String[] dictionaryArray = new String[]{"bash", "dash", "cash", "cbsh", "tbsh", "gash", "cbth", "thsh", "gahh", "chth", "thth"};
+    dictionary = Arrays.asList(dictionaryArray);
+    List<String> path = wordTransformer.transformWord(dictionary, "bash", "thsh");
+    for (String word : path) {
+      System.out.printf(word + ", ");
+    }
+    System.out.println();
   }
 }
