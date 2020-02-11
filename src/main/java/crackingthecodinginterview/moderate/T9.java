c1c90031d28172f1ad428bc99afde3a6e3e69fe3
Francis Pang <boyshawn@hotmail.com>
2020-02-11 11:51:10 +0800

    Additional review question Chapter 17 Hard 17.10 Majority Element - Redo

--- src/main/java/crackingthecodinginterview/moderate/T9.java
+++ src/main/java/crackingthecodinginterview/moderate/T9.java
@@ -1,75 +1,62 @@
 package crackingthecodinginterview.moderate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class T9 {
-  public List<String> getMatchingWordsFromDigits(String digits, Set<String> validWords) {
-    // Get the list of possible words first
-    Set<String> allPossibleWords = generatePossibleWordsFromDigits(digits);
-
-    // Use the word on the list and do a interception joint
-    allPossibleWords.retainAll(validWords);
-    return new ArrayList<>(allPossibleWords);
+  public List<String> getPossibleWords(List<String> validWords, int number) {
+    Map<String, List<String>> numberToWordsTable = new HashMap<>();
+    buildNumberToWordsTable(numberToWordsTable, validWords);
+    String numberString = Integer.toString(number);
+    return numberToWordsTable.getOrDefault(numberString, new ArrayList<>());
   }
 
-  private Set<String> generatePossibleWordsFromDigits(String digits) {
-    if (digits.length() == 0) {
-      return new HashSet<>();
-    }
-    char lastDigit = digits.charAt(digits.length() - 1);
-    if (!Character.isDigit(lastDigit)) {
-      throw new IllegalArgumentException(lastDigit + " inside " + digits + " is not a digit");
-    }
-    char[] charactersOfLastDigit = NumberPad.resolve(Integer.parseInt(Character.toString(lastDigit))).letters;
-    Set<String> subWordsSet = generatePossibleWordsFromDigits(digits.substring(0, digits.length() - 1));
-    if (charactersOfLastDigit.length == 0) {
-      return subWordsSet;
-    }
-    Set<String> possibleWords = new HashSet<>();
-    if (subWordsSet.isEmpty()) {
-      for (char lastPossibleCharacter : charactersOfLastDigit) {
-        possibleWords.add(Character.toString(lastPossibleCharacter));
+  private void buildNumberToWordsTable(Map<String, List<String>> numberToWordsTable, List<String> validWords) {
+    Map<Character, Integer> characterToNumberMap = new HashMap<>();
+    characterToNumberMap.put('a', 2);
+    characterToNumberMap.put('b', 2);
+    characterToNumberMap.put('c', 2);
+    characterToNumberMap.put('d', 3);
+    characterToNumberMap.put('e', 3);
+    characterToNumberMap.put('f', 3);
+    characterToNumberMap.put('g', 4);
+    characterToNumberMap.put('h', 4);
+    characterToNumberMap.put('i', 4);
+    characterToNumberMap.put('j', 5);
+    characterToNumberMap.put('k', 5);
+    characterToNumberMap.put('l', 5);
+    characterToNumberMap.put('m', 6);
+    characterToNumberMap.put('n', 6);
+    characterToNumberMap.put('o', 6);
+    characterToNumberMap.put('p', 7);
+    characterToNumberMap.put('q', 7);
+    characterToNumberMap.put('r', 7);
+    characterToNumberMap.put('s', 7);
+    characterToNumberMap.put('t', 8);
+    characterToNumberMap.put('u', 8);
+    characterToNumberMap.put('v', 8);
+    characterToNumberMap.put('w', 9);
+    characterToNumberMap.put('x', 9);
+    characterToNumberMap.put('y', 9);
+    characterToNumberMap.put('z', 9);
+    for (String word : validWords) {
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < word.length(); i++) {
+        char c = word.charAt(i);
+        int numericValue = characterToNumberMap.get(c);
+        sb.append(numericValue);
       }
-    } else {
-      for (String subWords : subWordsSet) {
-        for (char lastPossibleCharacter : charactersOfLastDigit) {
-          possibleWords.add(subWords.concat(Character.toString(lastPossibleCharacter)));
-        }
+      String numberSequence = sb.toString();
+      if (!numberToWordsTable.containsKey(numberSequence)) {
+        numberToWordsTable.put(numberSequence, new ArrayList<>());
       }
+      List<String> words = numberToWordsTable.get(numberSequence);
+      words.add(word);
     }
-    return possibleWords;
   }
 
-  public enum NumberPad {
-    ONE(1, new char[]{}),
-    TWO(2, new char[]{'a', 'b', 'c'}),
-    THREE(3, new char[]{'d', 'e', 'f'}),
-    FOUR(4, new char[]{'g', 'h', 'i'}),
-    FIVE(5, new char[]{'j', 'k', 'l'}),
-    SIX(6, new char[]{'m', 'n', 'o'}),
-    SEVEN(7, new char[]{'p', 'q', 'r', 's'}),
-    EIGHT(8, new char[]{'t', 'u', 'v'}),
-    NINE(9, new char[]{'w', 'x', 'y', 'z'}),
-    ZERO(0, new char[]{}),
-    ;
-    private final int number;
-    private final char[] letters;
-
-    NumberPad(int number, char[] letters) {
-      this.number = number;
-      this.letters = letters;
-    }
-
-    public static NumberPad resolve(int value) {
-      for (NumberPad numberPad : values()) {
-        if (numberPad.number == value) {
-          return numberPad;
-        }
-      }
-      return null;
-    }
-  }
 }
