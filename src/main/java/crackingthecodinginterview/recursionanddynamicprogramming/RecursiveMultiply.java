7f612abe1a06d925d6c1889df55198337c35ee04
Francis Pang <boyshawn@hotmail.com>
2020-02-13 14:57:37 +0800

    Concept and Algorithm Chapter 10 Sorting and Searching 10.1 Sorted Merge

--- src/main/java/crackingthecodinginterview/recursionanddynamicprogramming/RecursiveMultiply.java
+++ src/main/java/crackingthecodinginterview/recursionanddynamicprogramming/RecursiveMultiply.java
@@ -1,11 +1,34 @@
 package crackingthecodinginterview.recursionanddynamicprogramming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RecursiveMultiply {
   public int multiplyRecursively(int firstNumber, int secondNumber) {
-    if (secondNumber == 1) {
-      return firstNumber;
-    }
+    Map<Integer, Integer> table = new HashMap<>();
+    table.put(1, Math.max(firstNumber, secondNumber));
+    return multiply(Math.max(firstNumber, secondNumber), Math.min(firstNumber, secondNumber), table);
 
-    return firstNumber + multiplyRecursively(firstNumber, secondNumber - 1);
+  }
+
+  private int multiply(int number1, int number2, Map<Integer, Integer> table) {
+    if (table.containsKey(number2)) {
+      return table.get(number2);
+    }
+    int result;
+    if (number2 % 2 == 0) { // even
+      result = multiply(number1, number2 / 2, table) + multiply(number1, number2 / 2, table);
+    } else { // odd
+      result = multiply(number1, number2 / 2, table) + multiply(number1, number2 / 2, table) +
+          multiply(number1, 1, table);
+    }
+    table.put(number2, result);
+    return result;
+  }
+
+  public static void main(String[] args) {
+    RecursiveMultiply recursiveMultiply = new RecursiveMultiply();
+    int result = recursiveMultiply.multiplyRecursively(987561, 59866);
+    System.out.println("Answer=" + result);
   }
 }
--- src/main/java/crackingthecodinginterview/sortingandsearching/SortedMerge.java
+++ src/main/java/crackingthecodinginterview/sortingandsearching/SortedMerge.java
@@ -6,4 +6,55 @@ package crackingthecodinginterview.sortingandsearching;
  * Answer is in {@link leetcode.amazon.MergeTwoSortedLists}
  */
 public class SortedMerge {
+  public void mergeSortedArray(int[] arrayA, int[] arrayB) {
+    if (arrayB.length == 0) {
+      return;
+    }
+    if (arrayA.length == 0) {
+      for (int i = 0; i < arrayB.length; i++) {
+        arrayA[i] = arrayB[i];
+      }
+    }
+    // Find out the length of arrayA
+    // Assume that empty entries are filled with Integer.MIN_VALUE
+    int aSize = 0;
+    while (arrayA[aSize] != Integer.MIN_VALUE) {
+      aSize++;
+    }
+  }
+
+  private void mergeSortWhenArrayAIsInfiniteBig(int[] arrayA, int[] arrayB, int aSize) {
+    int aIterator = 0;
+    int bIterator = 0;
+    int aReverseStart = arrayA.length - 1;
+    int aReverseEnd = arrayA.length - 1;
+    while (bIterator < arrayB.length) {
+      if (aReverseEnd == arrayA.length - 1) {
+        if (arrayA[aIterator] <= arrayB[bIterator]) {
+        } else { // arrayA[aIterator] > arrayB[bIterator]
+          arrayA[aReverseEnd] = arrayA[aIterator];
+          aReverseEnd--;
+        }
+      } else {
+        arrayA[aReverseEnd] = arrayA[aIterator];
+        aReverseEnd++;
+        if (arrayA[aReverseStart] <= arrayB[bIterator]) {
+          arrayA[aIterator] = arrayA[aReverseStart];
+          aReverseStart++;
+        } else { // arrayA[aReverseStart] > arrayB[bIterator]
+          arrayA[aIterator] = arrayB[bIterator];
+          bIterator++;
+        }
+      }
+      aIterator++;
+    }
+    while (aIterator < aSize) {
+      arrayA[aReverseEnd++] = arrayA[aIterator];
+      arrayA[aIterator++] = arrayA[aReverseStart++];
+    }
+    while (aReverseStart != aReverseEnd) {
+      arrayA[aIterator++] = arrayA[aReverseStart++];
+    }
+  }
+
 }
