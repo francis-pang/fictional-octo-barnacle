d2264aa1360945447b5594d3bc466392a7eab680
Francis Pang <boyshawn@hotmail.com>
2020-02-17 09:50:21 +0800

    Concept and Algorithm Chapter 8 Recursion and Dynamic Programming 8.9 Parens
    commit 23ad63b13259ba9fe0d0e5afa13cfa9fbb8c5195 Wrong answer. Missing some examples.
    commit bc70197370c7f4d7ddf985b7aa527af0b8d6f3cd Correct the solution
    commit 4cf6d8fa54b68b4d51e5b3ec0ecd32d7642f7bea Replace original code from solution

--- src/main/java/leetcode/GenerateParentheses.java
+++ src/main/java/leetcode/GenerateParentheses.java
@@ -1,45 +1,51 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class GenerateParentheses {
-  public static void main(String[] args) {
-    Solution solution = new Solution();
-    System.out.println(solution.generateParenthesis(3));
+  public List<String> generateParenthesis(int n) {
+    Map<Integer, Set<String>> memoTable = new HashMap<>();
+    Set<String> single = new HashSet<>();
+    single.add("()");
+    memoTable.put(1, single);
+    Set<String> parenthesesSet = recursiveGenerateParentheses(n, memoTable);
+    return new ArrayList<>(parenthesesSet);
   }
 
-  static class Solution {
-    public List<String> generateParenthesis(int n) {
-      List<String> parenthesesCombinations = new ArrayList<>();
-      // Use n * 2 because we know that the maximum length of the string generated is "()" [length 2] * number of
-      // parentheses
-      char[] string = new char[n * 2];
-      populateParenthesisCombination(parenthesesCombinations, n, n, string, 0);
-      return parenthesesCombinations;
+  private Set<String> recursiveGenerateParentheses(int n, Map<Integer, Set<String>> memoTable) {
+    if (memoTable.containsKey(n)) {
+      return memoTable.get(n);
     }
-
-    // Initially the idea is to use StringBuilder, but because StringBuilder is passed by reference, the string will
-    // be added in the deeper call stack, and when returned, the stringBuilder passed into the next recursive call is
-    // already inaccurate, so it is best to stick to character array and int as index.
-    private void populateParenthesisCombination(List<String> combinations, int remainingLeftParenthesis,
-                                                int remainingRightParenthesis, char[] parenthese, int index) {
-      if (remainingLeftParenthesis < 0 || remainingRightParenthesis < 0 || remainingRightParenthesis < remainingLeftParenthesis) {
-        return;
-      }
-      if (remainingLeftParenthesis == 0 && remainingRightParenthesis == 0) {
-        combinations.add(new String(parenthese));
-      } else {
-        final char OPEN_PARENTHESIS = '(';
-        parenthese[index] = OPEN_PARENTHESIS;
-        populateParenthesisCombination(combinations, remainingLeftParenthesis - 1, remainingRightParenthesis,
-            parenthese, index + 1);
-
-        final char CLOSE_PARENTHESIS = ')';
-        parenthese[index] = CLOSE_PARENTHESIS;
-        populateParenthesisCombination(combinations, remainingLeftParenthesis, remainingRightParenthesis - 1,
-            parenthese, index + 1);
+    Set<String> parenthesesSet = new HashSet<>();
+    for (int i = 1; i < n; i++) {
+      int leftLength = i;
+      Set<String> leftParentheses = recursiveGenerateParentheses(leftLength, memoTable);
+      int rightLength = n - i;
+      Set<String> rightParentheses = recursiveGenerateParentheses(rightLength, memoTable);
+      for (String left : leftParentheses) {
+        rightParentheses.forEach(right -> parenthesesSet.add(left + right));
       }
     }
+    Set<String> oneSmallerSet = recursiveGenerateParentheses(n - 1, memoTable);
+    oneSmallerSet.forEach(e -> parenthesesSet.add("(" + e + ")"));
+    memoTable.put(n, parenthesesSet);
+    return parenthesesSet;
+  }
+
+  private static void printList(List<String> list) {
+    System.out.println("Total=" + list.size());
+    list.forEach(e -> System.out.printf(e + ","));
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    GenerateParentheses generateParentheses = new GenerateParentheses();
+    List<String> actual = generateParentheses.generateParenthesis(6);
+    printList(actual);
   }
 }
