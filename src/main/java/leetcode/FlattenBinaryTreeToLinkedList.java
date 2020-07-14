4ec90efc3981218aae12b476ebbe360c7c07c891
boyshawn <boyshawn@hotmail.com>
2020-07-14 17:53:24 +0800

    LC #114 Correct solution

--- src/main/java/leetcode/FlattenBinaryTreeToLinkedList.java
+++ src/main/java/leetcode/FlattenBinaryTreeToLinkedList.java
@@ -1,28 +1,41 @@
 package leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class FlattenBinaryTreeToLinkedList {
   public static void main(String[] args) {
     TreeNode root = TreeNodeCreator.createTreeNode("[1,2,5,3,4,null,6]");
     FlattenBinaryTreeToLinkedList flattenBinaryTreeToLinkedList = new FlattenBinaryTreeToLinkedList();
     flattenBinaryTreeToLinkedList.flatten(root);
+    System.out.println(root);
   }
 
   public void flatten(TreeNode root) {
-    root = preOrderTraverse(root, null);
+    if (root == null) {
+      return;
+    }
+    Queue<TreeNode> reversedOrderedNodes = new ArrayDeque<>();
+    preOrderTraverse(reversedOrderedNodes, root);
+    TreeNode predecessor = null;
+    while (!reversedOrderedNodes.isEmpty()) {
+      TreeNode node = reversedOrderedNodes.poll();
+      node.left = null;
+      node.right = null;
+      if (predecessor != null) {
+        predecessor.right = node;
+      }
+      predecessor = node;
+    }
   }
 
-  private TreeNode preOrderTraverse(TreeNode node, TreeNode predecessor) {
-    TreeNode newNode = new TreeNode(node.val);
-    if (predecessor != null) {
-      predecessor.right = newNode;
-    }
-    TreeNode newLeftNode = null;
+  private void preOrderTraverse(Queue<TreeNode> reversedOrderedNodes, TreeNode node) {
+    reversedOrderedNodes.add(node);
     if (node.left != null) {
-      newLeftNode = preOrderTraverse(node.left, newNode);
+      preOrderTraverse(reversedOrderedNodes, node.left);
     }
     if (node.right != null) {
-      preOrderTraverse(node.right, newLeftNode);
+      preOrderTraverse(reversedOrderedNodes, node.right);
     }
-    return newNode;
   }
 }
